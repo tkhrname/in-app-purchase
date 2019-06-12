c8c1652d12ea1aafb7c63d97faa0e27120b2de0f
Takehiro Watanabe <takehiro.watanabe.224@gmail.com>
2019-06-12 09:37:54 +0900

    Initial Commit

---
+++ SubscriptionApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SubscriptionApp
+//
+//  Created by 渡邊丈洋 on 2019/06/12.
+//  Copyright © 2019 渡邊丈洋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
